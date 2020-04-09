999a07d6bb17fb473630ec9a5df36cbcd2f456b0
RicardoWorf <steve4137@gmail.com>
2020-04-08 22:26:32 -0300

    Add new route for edit page

--- web/src/routes.js
+++ web/src/routes.js
@@ -5,6 +5,7 @@ import Logon from './pages/Logon';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
 import NewTasks from './pages/NewTasks';
+import EditionTasks from './pages/EditionTasks';
 
 export default function Routes() {
 	return (
@@ -14,6 +15,7 @@ export default function Routes() {
 				<Route path="/register" component={Register}/>
 				<Route path="/profile" exact component={Profile}/>
 				<Route path="/profile/tasks/new" component={NewTasks}/>
+				<Route path="/profile/tasks/edition" component={EditionTasks}/>
 			</Switch>
 		</BrowserRouter>
 	);
