ba07bac92b698814878c035791e0a22a359bfc11
RicardoWorf <steve4137@gmail.com>
2020-04-09 22:40:58 -0300

    Modification for too Search

--- backend/src/controllers/SearchController.js
+++ backend/src/controllers/SearchController.js
@@ -3,9 +3,11 @@ const connection = require('../database/connection');
 module.exports = {
 	async index(req, res) {
 		const { search } = req.body;
+		const user_id = req.headers.authorization;
 
 		const tasks = await connection('tasks')
 			.where('title', 'like', `%${search}%`)
+			.whereIn('user_id', '=', user_id)
 			.select('*');
 
 		return res.json(tasks);
