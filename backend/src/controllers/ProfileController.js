cb89e0bf650aeb8eed1e1f0c640b034a7d64d41b
RicardoWorf <steve4137@gmail.com>
2020-04-09 22:40:27 -0300

    Modifications for Search

--- backend/src/controllers/ProfileController.js
+++ backend/src/controllers/ProfileController.js
@@ -3,11 +3,20 @@ const connection = require('../database/connection');
 module.exports = {
 	async index(req, res) {
 		const user_id = req.headers.authorization;
+		const search = req.body;
 
+		if(search === undefined) {
+			const tasks = await connection('tasks')
+				.where('user_id', user_id)
+				.select('*');
+
+			return res.json(tasks);
+		}
 		const tasks = await connection('tasks')
-			.where('user_id', user_id)
+			.where('title', 'like', `%${search}%`)
+			.whereIn('user_id', user_id)
 			.select('*');
 
-		return res.json(tasks);
+		return res.json(tasks);	
 	}
 }
