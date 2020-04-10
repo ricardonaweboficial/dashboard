e7f24e110bcb58d8f862365a92ac6fc138a386b6
RicardoWorf <steve4137@gmail.com>
2020-04-09 22:42:17 -0300

    Integration for API

--- web/src/pages/EditionTasks/index.js
+++ web/src/pages/EditionTasks/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import { Link, useHistory, useParams } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import api from '../../services/api';
 
@@ -8,10 +8,31 @@ import notebookImage from '../../assets/support-notes.svg';
 import './styles.css';
 
 export default function Profile() {
+	const { title, description, id, user_id } = useParams();
+	const history = useHistory();
+
+	const [ titleModify, setTitleModify ] = useState(title);
+	const [ descriptionModify, setDescriptionModify ] = useState(description);
 
 	async function handleEdition(e) {
 		e.preventDefault();
-		return '';
+
+		try {
+			const data = {
+				title: titleModify,
+				description: descriptionModify
+			}
+			await api.put(`tasks/${id}`,data, {
+				headers: {
+					authorization: user_id
+				}
+			});
+
+			return history.push('/profile');
+		} catch (err) {
+			alert('Erro na modificação da tarefa.');
+		}
+
 	}
 
 	return (
@@ -19,10 +40,14 @@ export default function Profile() {
 
 			<form onSubmit={handleEdition}>
 				<input 
+					value={titleModify}
+					onChange={e => setTitleModify(e.target.value)}
 					placeholder="Título"
 					required
 				/>
 				<textarea 
+					value={descriptionModify}
+					onChange={e => setDescriptionModify(e.target.value)}
 					placeholder="Descrição"
 					required
 				/>
