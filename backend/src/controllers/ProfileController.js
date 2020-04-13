842a438104ce0e21dc5633da4831e4764675f1a7
RicardoWorf <steve4137@gmail.com>
2020-04-12 21:48:42 -0300

    Add Search of to-do

--- backend/src/controllers/ProfileController.js
+++ backend/src/controllers/ProfileController.js
@@ -2,21 +2,23 @@ const connection = require('../database/connection');
 
 module.exports = {
 	async index(req, res) {
-		const user_id = req.headers.authorization;
-		const search = req.body;
-
-		if(search === undefined) {
+		const user_id = req.headers.authorization;	
+		const { search } = req.query;
+		
+		if(search) {
 			const tasks = await connection('tasks')
+				.where('title', 'like', `%${search}%`)
 				.where('user_id', user_id)
 				.select('*');
 
+			return res.json(tasks);	
+		} else if (!search) {
+			const tasks = await connection('tasks')
+				.where('user_id', user_id)
+				.select('*');
 			return res.json(tasks);
-		}
-		const tasks = await connection('tasks')
-			.where('title', 'like', `%${search}%`)
-			.whereIn('user_id', user_id)
-			.select('*');
+		}	
 
-		return res.json(tasks);	
+		return res.json({ error: 'Error search this to-do' });
 	}
 }
