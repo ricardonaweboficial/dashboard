a0f9155097000ea06d63746bc1275bd8620f21a1
RicardoWorf <steve4137@gmail.com>
2020-04-24 21:10:54 -0300

    New Route for page User Edition

--- web/src/routes.js
+++ web/src/routes.js
@@ -3,7 +3,9 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Logon from './pages/Logon';
 import Register from './pages/Register';
+
 import Profile from './pages/Profile';
+import User from './pages/User';
 import NewTasks from './pages/NewTasks';
 import EditionTasks from './pages/EditionTasks';
 
@@ -14,8 +16,10 @@ export default function Routes() {
 				<Route path="/" exact component={Logon}/>
 				<Route path="/register" component={Register}/>
 				<Route path="/profile" exact component={Profile}/>
+				<Route path="/profile/user" component={User}/>
 				<Route path="/profile/tasks/new" component={NewTasks}/>
 				<Route path="/profile/tasks/edition/:id" component={EditionTasks}/>
+
 			</Switch>
 		</BrowserRouter>
 	);
